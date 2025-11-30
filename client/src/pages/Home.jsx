116634fd38454242926db8a684884e714437f5d8
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-11-29 22:28:57 -0800

    Trailer Display section completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FearuredSection from '../components/FearuredSection'
+import TrailerSection from '../components/TrailerSection'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <FearuredSection/>
+      <TrailerSection/>
     </>
   )
 }
