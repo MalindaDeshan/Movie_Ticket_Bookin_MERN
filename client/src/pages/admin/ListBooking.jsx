a678b584a774c05c7daf35fd87c3b30f95e3aca6
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-12-10 09:09:07 -0800

    List-Shows page is completed for admin

--- client/src/pages/admin/ListBooking.jsx
+++ client/src/pages/admin/ListBooking.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
+import { dummyBookingData } from '../../assets/assets';
+import Loading from '../../components/Loading';
+import Title from '../../components/admin/Title';
 
 const ListBooking = () => {
-  return (
+  const currency = import.meta.env.VITE_CURRENCY;
+  const [bookings,setBookings] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+  const fetchAllBookings = async () => {
+    setBookings(dummyBookingData)
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchAllBookings();
+  },[]);
+
+  return !loading ?(
     <div>
-      
+      <Title text1="List" text2="Bookings"/>
     </div>
-  )
+  ):<Loading/>;
 }
 
 export default ListBooking
