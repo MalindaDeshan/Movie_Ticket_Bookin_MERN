fc8e136a6901ae7f7dfd43f90a4f689f5db9817c
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-12-12 15:13:58 -0800

    User data Update function done

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -32,9 +32,27 @@ const syncUserDeletion = inngest.createFunction(
     }
 )
 
+//Inngest function to update user data to a database
+const syncUserUpdate = inngest.createFunction(
+    {id: 'update-user-from-clerk'},
+    {event: 'clerk/user.updated'},
+
+    async ({event}) => {
+        const {id,first_name,last_name,email_addresses,image_url} = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + " " + last_name,
+            Image: image_url
+        }
+        await User.findByIdAndUpdate(id, userData);
+    }
+)
+
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
     syncUserCreation,
-    syncUserDeletion
+    syncUserDeletion,
+    syncUserUpdate
 ];
