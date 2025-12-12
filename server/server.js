96ab45633a84046f3dd138344e505c66ee6816f2
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-12-12 14:31:43 -0800

    Setup clerk and Inngest into Backend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config.js';
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ await connectDB();
 //Middlewares
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 app.get('/', (req, res) => {
     res.send('Server is running');
