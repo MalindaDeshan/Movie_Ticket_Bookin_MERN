cfb9c69106fbb8f4046fbd4754148a22abfa640f
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-12-18 08:55:37 -0800

    Add middleware to routes

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { addShow, getNowPlayingMovies } from '../controllers/showController.js';
+import { projectAdmin } from '../middleware/auth.js';
 
 const showRouter = express.Router();
 
-showRouter.get('/now-playing',getNowPlayingMovies)
-showRouter.post('/add',addShow)
+showRouter.get('/now-playing',projectAdmin, getNowPlayingMovies)
+showRouter.post('/add',projectAdmin, addShow)
 
 export default showRouter;
